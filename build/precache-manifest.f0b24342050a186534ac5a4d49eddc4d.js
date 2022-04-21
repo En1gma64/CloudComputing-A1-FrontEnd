daa255ba7059db815311f565079fbaf571ba6182
En1gma64 <s3823697@student.rmit.edu.au>
2022-04-22 00:36:23 +1000

    Re-Configure Files

---
+++ build/precache-manifest.f0b24342050a186534ac5a4d49eddc4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92525982b49746f41592be258814586c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2171eceba7c793e6773",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "9bf8f1f8a41ddc6f5f61",
+    "url": "/static/css/main.7d62cbb7.chunk.css"
+  },
+  {
+    "revision": "f2171eceba7c793e6773",
+    "url": "/static/js/2.d6ab7694.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.d6ab7694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf8f1f8a41ddc6f5f61",
+    "url": "/static/js/main.8fc19755.chunk.js"
+  },
+  {
+    "revision": "49c99c066af9349ce9de",
+    "url": "/static/js/runtime-main.23554a8f.js"
+  }
+]);
